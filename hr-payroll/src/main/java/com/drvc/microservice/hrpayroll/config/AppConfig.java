22c6806c2fbc7e675981ba494ed534bb62fc0f2d
diegorvcassiano <diegorvcassiano@gmail.com>
2022-03-26 11:17:56 -0300

    RestTempale implementation and intercommunication between hr-payroll to hr-worker

---
+++ hr-payroll/src/main/java/com/drvc/microservice/hrpayroll/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.drvc.microservice.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate getRestTemplateInstance() {
+		return new RestTemplate();
+	}
+}
--- hr-payroll/src/main/java/com/drvc/microservice/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/drvc/microservice/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.drvc.microservice.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.drvc.microservice.hrpayroll.entities.Payment;
+import com.drvc.microservice.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
-	
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 100.0, days);
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
