d5edb8be6e9d7acbee57558db653e44fc3a71151
diegorvcassiano <diegorvcassiano@gmail.com>
2022-03-31 22:04:58 -0300

    Login, JWT token and oAuth process

--- hr-oauth/src/main/java/com/drvc/microservice/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/drvc/microservice/hroauth/entities/User.java
@@ -1,16 +1,23 @@
 package com.drvc.microservice.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable {
-	private static final long serialVersionUID = 1L;	
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
 	private String email;
-	private String password;	private Set<Role> roles = new HashSet<Role>();
+	private String password;
+	private Set<Role> roles = new HashSet<Role>();
 
 	public User() {
 	}
@@ -61,6 +68,36 @@ public class User implements Serializable {
 	}
 
 	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
@@ -76,5 +113,4 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
--- hr-oauth/src/main/java/com/drvc/microservice/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/drvc/microservice/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.drvc.microservice.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.drvc.microservice.hroauth.entities.User;
 import com.drvc.microservice.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,6 +28,20 @@ public class UserService {
 		}
 		
 		logger.info("Email %s found", email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			logger.error("loadUserByUsername - Email not found! Email =  " + username);
+			throw new UsernameNotFoundException("Email not found! Email =  " + username);
+		}
+		
+		logger.info("Email found: " + username);
 		
 		return user;
 	}
